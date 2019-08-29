ce44338fc62b7aec59f3761c1deecfbbae489fda
yyz <yyz163.com>
2019-08-29 12:36:45 +0800

    使用mybatis逆向工程生成Mapper接口文件

---
+++ src/main/java/com/usthe/bootshiro/dao/AuthUserRoleDao.java
@@ -0,0 +1,17 @@
+package com.usthe.bootshiro.dao;
+
+import com.usthe.bootshiro.domain.bo.AuthUserRole;
+
+public interface AuthUserRoleDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(AuthUserRole record);
+
+    int insertSelective(AuthUserRole record);
+
+    AuthUserRole selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(AuthUserRole record);
+
+    int updateByPrimaryKey(AuthUserRole record);
+}
